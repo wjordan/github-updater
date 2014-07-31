32028267577e3a3a91f3e646bafe185493d64093
Will Jordan <will@code.org>
2014-07-30 19:14:27 -0700

    check repo and branch

--- lib/github-updater.rb
+++ lib/github-updater.rb
@@ -12,11 +12,14 @@ module GithubUpdater
         payload_body = request.body.read
         verify_signature(payload_body)
         payload = JSON.parse(payload_body)
+        halt 500 unless current_repo?(payload['repository']['html_url'])
+        halt 500 unless current_branch?(payload['ref'])
         (GithubUpdater::instance_variable_get(:@webhooks) || []).each do |block|
           block.call(payload)
         end
         status 200
-      rescue
+      rescue => e
+        puts "error:#{e}"
         halt 500
       end
     end
@@ -24,6 +27,16 @@ module GithubUpdater
     def verify_signature(payload_body)
       signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET_TOKEN'], payload_body)
       return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    end
+
+    def current_repo?(url)
+      git_url = `git ls-remote --get-url`.strip.gsub(/git@github.com:/, 'https://github.com/')
+      url == git_url
+    end
+
+    def current_branch?(branch)
+      git_branch = `git symbolic-ref HEAD`.strip
+      git_branch == branch
     end
 
   end
