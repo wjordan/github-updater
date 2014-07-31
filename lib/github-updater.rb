4c37de472a67f81b48110dc9fc9d2fbe5d2a3ff0
Will Jordan <will@code.org>
2014-07-31 09:35:19 -0700

    refactoring

--- lib/github-updater.rb
+++ lib/github-updater.rb
@@ -10,33 +10,30 @@ module GithubUpdater
       begin
         request.body.rewind
         payload_body = request.body.read
-        verify_signature(payload_body)
+        halt 500 unless verify_signature(payload_body)
         payload = JSON.parse(payload_body)
-        halt 500 unless current_repo?(payload['repository']['html_url'])
-        halt 500 unless current_branch?(payload['ref'])
+        halt 500 unless current_repo == payload['repository']['html_url'] && current_branch == payload['ref']
         (GithubUpdater::instance_variable_get(:@webhooks) || []).each do |block|
           block.call(payload)
         end
         status 200
       rescue => e
-        puts "error:#{e}"
+        puts "Error:#{e}"
         halt 500
       end
     end
 
     def verify_signature(payload_body)
       signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET_TOKEN'], payload_body)
-      return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+      Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
     end
 
-    def current_repo?(url)
-      git_url = `git ls-remote --get-url`.strip.gsub(/git@github.com:/, 'https://github.com/')
-      url == git_url
+    def current_repo
+      `git ls-remote --get-url`.strip.gsub(/git@github.com:/, 'https://github.com/')
     end
 
-    def current_branch?(branch)
-      git_branch = `git symbolic-ref HEAD`.strip
-      git_branch == branch
+    def current_branch
+      `git symbolic-ref HEAD`.strip
     end
 
   end
@@ -46,4 +43,4 @@ module GithubUpdater
   end
 end
 
-require 'github-updater/railtie' if defined? Rails
+require 'github-updater/railtie' if defined? Rails::Railtie
